9c2ebe5bfc665b039ed0466e49c6d73c3c60a1c6
Snot1k <arturvika123@gmail.com>
2023-09-23 13:17:31 +0300

    add post list

--- BlogApp/views.py
+++ BlogApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 def index(req):
-    return render(request=req, template_name="blogapp/index.html")
+    post_list = Post.objects.all()
+    return render(request=req, template_name="blogapp/index.html", context={"posts" : post_list})
