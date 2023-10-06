1d47cf0926f15c5ae2a98b4760471151dc78302f
Snot1k <117514040+Snot1k@users.noreply.github.com>
2023-10-06 10:48:36 +0300

    add 2 comments to models.py

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -3,13 +3,13 @@ from django.urls import reverse
 # Create your models here.
 class Category(models.Model):
     category_name = models.CharField(max_length=25)
-
+# We create model Post
 class Post(models.Model):
     post_header = models.CharField(max_length=50)
     post_text = models.TextField()
     author = models.CharField(max_length=50)
     datetime = models.DateTimeField()
     category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default="no category")
-
+# This function get post url
     def get_absolute_url(self):
         return reverse("post-details", args=[str(self.id)])
