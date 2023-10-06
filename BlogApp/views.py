96630d39f1f99f532ff34f3a40f84670c6668285
Snot1k <117514040+Snot1k@users.noreply.github.com>
2023-10-06 10:50:08 +0300

    add comment to views.py

--- BlogApp/views.py
+++ BlogApp/views.py
@@ -5,6 +5,7 @@ from django.views.generic import DetailView
 def index(req):
     post_list = Post.objects.all()
     return render(request=req, template_name="blogapp/index.html", context={"posts" : post_list})
+#create class for view post
 class PostDetailView(DetailView):
     template_name = "blogapp/post-detail.html"
     
