608805e8e72dcf35fd64a85d707111117c8060f4
Snot1k <arturvika123@gmail.com>
2023-10-28 13:32:01 +0300

    Add cat and auth links

--- BlogApp/views.py
+++ BlogApp/views.py
@@ -1,12 +1,46 @@
+from typing import Any
 from django.shortcuts import render
-from .models import Post
-from django.views.generic import DetailView
+from .models import Post, Category
+from django.views.generic import DetailView, ListView
 # Create your views here.
 def index(req):
     post_list = Post.objects.all()
+    if req.GET.get("author", False):
+        author = req.GET.get("author", False)
+        post_list = Post.objects.filter(author__exact=author)
+
+    if req.GET.get("category", False):
+        category = req.GET.get("category", False)
+        post_list = Post.objects.filter(category__category_name=category)
+    
     return render(request=req, template_name="blogapp/index.html", context={"posts" : post_list})
 #create class for view post
 class PostDetailView(DetailView):
     template_name = "blogapp/post-detail.html"
     
     model=Post
+
+class CategoryList(ListView):
+    template_name="blogapp/category_list.html"
+    model = Category
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["category_list"] = self.get_queryset()
+
+        return context
+    
+class AuthorList(ListView):
+    template_name="blogapp/author_list.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["author_list"] = []
+
+        for  post in self.get_queryset():
+            if not post.author in context["author_list"]:
+                context["author_list"].append(post.author)
+
+        return context
